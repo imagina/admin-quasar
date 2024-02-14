b852207ccb0114b109c6aa8cb0aaa3047d2f74cf
wilmerace <wilmerc@imaginacolombia.com>
2024-02-14 17:38:23 -0500

    utils.ts file is created

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,39 +1,54 @@
-// translations.js
 import moment from 'moment';
+import { ref, computed } from 'vue';
 
-export default function translation(i18n) {
-  const trc = (num, lang) => {
-    return i18n.global.n(num, 'currency', lang);
-  };
+class Translator {
+  constructor(i18n) {
+    this.i18n = i18n;
+    this.trc = this.trc.bind(this);
+    this.trn = this.trn.bind(this);
+    this.tr = this.tr.bind(this);
+    this.trp = this.trp.bind(this);
+    this.trd = this.trd.bind(this);
+    this.trdT = this.trdT.bind(this);
+  }
 
-  const trn = (num, type) => {
+  trc(num, lang) {
+    return this.i18n.global.n(num, 'currency', lang);
+  }
+
+  trn(num, type) {
     if (type === 'percent') num /= 100;
-    return type ? i18n.global.n(num, type) : i18n.n(num);
-  };
+    return type ? this.i18n.global.n(num, type) : this.i18n.n(num);
+  }
 
-  const tr = (key, params = {}) => {
-    return i18n.global.tc(key, 1, params);
-  };
+  tr(key, params = {}) {
+    return this.i18n.global.tc(key, 1, params);
+  }
 
-  const trp = (key, params = {}) => {
-    return i18n.global.tc(key, 2, params);
-  };
+  trp(key, params = {}) {
+    return this.i18n.global.tc(key, 2, params);
+  }
 
-  const trd = (date, params = { type: 'short', fromUTC: false }) => {
-    if (params.fromUTC) date = moment(date).local().format('YYYY-MM-DD HH:mm:ss');
-    return i18n.global.d(moment(date, 'YYYY-MM-DD HH:mm:ss').toDate(), params.type);
-  };
+  trd(date, params = { type: 'short', fromUTC: false }) {
+    if (params.fromUTC) {
+      date = moment(date).local().format('YYYY-MM-DD HH:mm:ss');
+    }
+    return this.i18n.global.d(moment(date, 'YYYY-MM-DD HH:mm:ss').toDate(), params.type);
+  }
 
-  const trdT = (date, format = 'MMMM, DD, YYYY HH:mm') => {
+  trdT(date, format = 'MMMM, DD, YYYY HH:mm') {
     return moment(date).format(format);
-  };
-
-  return {
-    trc,
-    trn,
-    tr,
-    trp,
-    trd,
-    trdT
-  };
+  }
 }
+
+const stateTrans = ref(null);
+const data = computed(() => ({
+  get trans() {
+    return stateTrans.value;
+  },
+  set trans(value) {
+    stateTrans.value = new Translator(value);
+  }
+})).value;
+export default data;
+
