a4ef2fbfce007c9a9be9ea2b40ae0ae4bb10b165
msolanogithub <msolano@imaginacolombia.com>
2024-02-16 09:13:14 -0500

    added useLocalTranslations config

--- src/setup/app.js
+++ src/setup/app.js
@@ -1,10 +1,10 @@
 //Get package information
-import pjson from '../../package.json'
+import pjson from '../../package.json';
 //Parse domain to know from where is loading
-let tagsToParceHost = ['http://', 'https://', ':8080', ':3000', 'www.']
-let host = window.location.href
-tagsToParceHost.forEach(tagToReplace => host = host.replace(tagToReplace, ''))
-let loadFrom = host.split('/')[1]
+let tagsToParceHost = ['http://', 'https://', ':8080', ':3000', 'www.'];
+let host = window.location.href;
+tagsToParceHost.forEach(tagToReplace => host = host.replace(tagToReplace, ''));
+let loadFrom = host.split('/')[1];
 
 export default {
   baseUrl: 'https://ibuilder.ozonohosting.com',
@@ -12,7 +12,7 @@ export default {
   mode: (loadFrom && (['iadmin', 'ipanel'].indexOf(loadFrom) != -1)) ? loadFrom : 'iadmin',//Define load mode
   validateModeAccess: true,//Define if validate app mode access
   forceRoleAndDepartment: false,//Force to select role and department
-  //useLocalTranslations:true,
+  //useLocalTranslations: true,//Define from where use the translations
   //UI Languages
   languages: {
     default: 'es',
@@ -87,7 +87,5 @@ export default {
     icon: 'fas fa-home',
     color: 'primary',
     route: 'app.home'
-  },
-  //Define from where use the translations
-  useLocalTranslations: true
-}
+  }
+};
