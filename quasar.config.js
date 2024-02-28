1683b4ca2571be72d1acfa596805b417afd17ca2
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-02-28 18:35:54 -0500

    - typo: removed https

--- quasar.config.js
+++ quasar.config.js
@@ -111,7 +111,7 @@ module.exports = configure(function(ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
     devServer: {
       server: {
-        type: 'https'
+        type: 'http'
       },
       port: 8080,
       open: true // opens browser window automatically
